77e2467b6c2818551a95de21d30145105eab4b82
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-08-04 17:02:26 +0530

    Update and rename UserRegistration.java to UserRegistrationLambda.java

---
+++ src/main/java/com/Bridgelabz/UserRegistrationLambda.java
@@ -0,0 +1,23 @@
+package com.Lambda;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+
+public class UserRegistrationLambda {
+    public static void main(String[] args) {
+
+        //UC1: first name validation
+        List<String> fname = Arrays.asList("Srushti", "Rasika", "Ruchika", "JidnyA");
+        final Pattern pattern1 = Pattern.compile("^[A-Z][a-z]{3,15}$");
+
+        // Using regex with lambda expression
+        List<String> fname_result = fname.stream().filter(p -> pattern1.matcher(p).find()).collect(Collectors.<String>toList());
+
+        //printing result
+        fname_result.forEach(System.out::println);
+
+    }
+}
