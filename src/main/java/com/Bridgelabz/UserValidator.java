ec17e6bf48bfc4db332a140da9ecd6f59e3ebc3c
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-08-04 17:11:59 +0530

    Update UserValidator.java

--- src/main/java/com/Bridgelabz/UserValidator.java
+++ src/main/java/com/Bridgelabz/UserValidator.java
@@ -1,17 +1,1 @@
-package com.Bridgelabz;
-import java.util.regex.Pattern;
 
-public class UserValidator {
-    private static final String FIRST_NAME_PATTERN = "^[A-Z] {1} [a-z] {2,} $";
-    private static final String LAST_NAME_PATTERN = "^[A-Z] {1} [A-Z] {2,} $";
-
-    public boolean validateFirstName(String fname) {
-        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
-        return pattern.matcher(fname).matches();
-    }
-    public boolean validateLastName(String lname) {
-        Pattern pattern = Pattern.compile(LAST_NAME_PATTERN);
-        return pattern.matcher(lname).matches();
-    }
-
-}
