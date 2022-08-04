683c235f3e006eb8d914ab491d9f2caef1d49188
srushtimane27 <107099158+srushtimane27@users.noreply.github.com>
2022-08-04 17:09:56 +0530

    Update UserRegistrationLambda.java

--- src/main/java/com/Bridgelabz/UserRegistrationLambda.java
+++ src/main/java/com/Bridgelabz/UserRegistrationLambda.java
@@ -18,6 +18,37 @@ public class UserRegistrationLambda {
 
         //printing result
         fname_result.forEach(System.out::println);
+        
+        //last name validation
+			List<String> lname = Arrays.asList("Pawar", "Ghadi","pa1il","g0re");
+			final Pattern pattern2 = Pattern.compile("^[A-Z][a-z]{3,15}$");
+			
+			// Using regex with lambda expression
+			List<String> lname_result = lname.stream().filter(p -> pattern2.matcher(p).find()).collect(Collectors.<String>toList());
+			
+			//printing result
+			lname_result.forEach(System.out::println);
+			
+			//email validation
+			List<String> mail = Arrays.asList("shwet.ps@gmail.com","ihi@i@hgam.co","flip.12@kore.co.in");
+			final Pattern pattern3 = Pattern.compile("^([a-zA-Z0-9.]+)([0-9a-zA-Z]{0,1}@([a-zA-Z0-9.]+([a-zA-Z]{2,3}))+([a-z]{0,2}))$");
+			
+			// Using regex with lambda expression
+			List<String> mail_result = mail.stream().filter(p -> pattern3.matcher(p).find()).collect(Collectors.<String>toList());
+			
+			//printing result
+			mail_result.forEach(System.out::println);
+			
+			//mobile validation
+			List<String> mob = Arrays.asList("+91 9823792341","929876543156","+01 98765216382");
+			final Pattern pattern4 = Pattern.compile("^[+]\\d{2}[ ]\\d{10}$");
+			
+			// Using regex with lambda expression
+			List<String> mob_result = mob.stream().filter(p -> pattern4.matcher(p).find()).collect(Collectors.<String>toList());
+			
+			//printing result
+			mob_result.forEach(System.out::println);
+			
 
     }
 }
